3a52d7ccd42d4022e5d20a2b7d2402c9a7d4409b
AlexandruDaniel <barbualex.daniel2004@gmail.com>
2025-05-24 12:55:32 +0300

    Removed debug files, also i now have a square not a triangle
    Signed-off-by: AlexandruDaniel <barbualex.daniel2004@gmail.com>

--- NewTrainingFramework/NewTrainingFramework.cpp
+++ NewTrainingFramework/NewTrainingFramework.cpp
@@ -24,18 +24,29 @@ int Init ( ESContext *esContext )
 	glClearColor ( 0.0f, 0.0f, 0.0f, 0.0f );
 
 	//triangle data (heap)
-	Vertex verticesData[3];
+	Vertex verticesData[6];
 
+	// First Triangle
 	// Set position of vertices
-	verticesData[0].pos.x =  0.0f;  verticesData[0].pos.y =  0.5f;  verticesData[0].pos.z =  0.0f;
-	verticesData[1].pos.x = -0.5f;  verticesData[1].pos.y = -0.5f;  verticesData[1].pos.z =  0.0f;
-	verticesData[2].pos.x =  0.5f;  verticesData[2].pos.y = -0.5f;  verticesData[2].pos.z =  0.0f;
+	verticesData[0].pos.x = -0.5f;  verticesData[0].pos.y =  0.5f;  verticesData[0].pos.z =  0.0f;
+	verticesData[1].pos.x =  0.5f;  verticesData[1].pos.y =  0.5f;  verticesData[1].pos.z =  0.0f;
+	verticesData[2].pos.x = -0.5f;  verticesData[2].pos.y = -0.5f;  verticesData[2].pos.z =  0.0f;
 
 	// Set Color of vertices
-	verticesData[0].color.x = 1.0f; verticesData[0].color.y = 0.0f; verticesData[0].color.z = 0.0f;
-	verticesData[1].color.x = 0.0f; verticesData[1].color.y = 1.0f; verticesData[1].color.z = 0.0f;
-	verticesData[2].color.x = 0.0f; verticesData[2].color.y = 0.0f; verticesData[2].color.z = 1.0f;
-	
+	verticesData[0].color.x = 1.0f; verticesData[0].color.y = 0.0f; verticesData[0].color.z = 0.0f;	 // R
+	verticesData[1].color.x = 0.0f; verticesData[1].color.y = 1.0f; verticesData[1].color.z = 0.0f;  // G
+	verticesData[2].color.x = 0.0f; verticesData[2].color.y = 0.0f; verticesData[2].color.z = 1.0f;	 // B
+
+	// First Triangle
+	// Set position of vertices
+	verticesData[3].pos.x =  0.5f;  verticesData[3].pos.y = -0.5f;  verticesData[3].pos.z =  0.0f;
+	verticesData[4].pos.x =  0.5f;  verticesData[4].pos.y =  0.5f;  verticesData[4].pos.z =  0.0f;
+	verticesData[5].pos.x = -0.5f;  verticesData[5].pos.y = -0.5f;  verticesData[5].pos.z =  0.0f;
+
+	// Set Color of vertices
+	verticesData[3].color.x = 1.0f; verticesData[3].color.y = 1.0f; verticesData[3].color.z = 0.0f;	 // Y
+	verticesData[4].color.x = 0.0f; verticesData[4].color.y = 1.0f; verticesData[4].color.z = 0.0f;  // G
+	verticesData[5].color.x = 0.0f; verticesData[5].color.y = 0.0f; verticesData[5].color.z = 1.0f;	 // B
 	
 	
 	//buffer object
@@ -88,7 +99,7 @@ void Draw ( ESContext *esContext )
 		glUniformMatrix4fv(myShaders.modelViewPerspectiveUniform, 1, GL_FALSE, &MVP.m[0][0]);
 	}
 
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
